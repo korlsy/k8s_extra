06e652ca49c84f7cad709d91b0722f3c68771bb2
sangyeol.lee <korlsy@gmail.com>
2025-08-17 13:40:10 +0900

    c

--- argoCD/etc_dev_node_appjs/image/build.sh
+++ argoCD/etc_dev_node_appjs/image/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # minikube image load lsy-app/node-appjs
-# /home/sangyeol/github_clone/k8s_extra/argoCD/etc_dev_node_appjs/image
+#
+# cd /home/sangyeol/github_clone/k8s_extra/argoCD/etc_dev_node_appjs/image
+#
+# eval $(minikube docker-env)
+# docker images 해서 나오는지 확인.
+# docker images|grep app
 
 docker build -t lsy-app/node-appjs .
